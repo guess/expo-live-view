d9843d34051d1559e60ba91c9ccc3c3d4bd0e72c
Steve Strates <steve@strates.io>
2024-08-29 22:05:23 -0400

    feat: allow for custom components to be used for form fields

--- src/forms/FormField.tsx
+++ src/forms/FormField.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react';
 import { observer } from 'mobx-react-lite';
 import { StyleSheet, Text, View } from 'react-native';
+import type { TextStyle, ViewStyle } from 'react-native';
 
 export type FormFieldSpec = {
   getValue: () => any;
@@ -16,23 +17,67 @@ type FormFieldProps = {
     onChange: (value: any) => void,
     errors: string[] | undefined
   ) => ReactNode;
+  renderLabel?: (label: string) => ReactNode;
+  renderError?: (error: string, index: number) => ReactNode;
+  containerStyle?: ViewStyle;
+  labelStyle?: TextStyle;
+  errorStyle?: TextStyle;
 };
 
 export const FormField = observer(
-  ({ field, label, children }: FormFieldProps) => {
+  ({
+    field,
+    label,
+    children,
+    renderLabel,
+    renderError,
+    containerStyle,
+    labelStyle,
+    errorStyle,
+  }: FormFieldProps) => {
+    const labelRenderer =
+      renderLabel ||
+      ((formLabel: string) => (
+        <FormFieldLabel style={labelStyle}>{formLabel}</FormFieldLabel>
+      ));
+
+    const errorRenderer =
+      renderError ||
+      ((formError: string, index: number) => (
+        <FormFieldError key={index} style={errorStyle}>
+          {formError}
+        </FormFieldError>
+      ));
+
     return (
-      <View style={styles.fieldContainer}>
-        {label && <Text style={styles.label}>{label}</Text>}
+      <View style={[styles.fieldContainer, containerStyle]}>
+        {label && labelRenderer(label)}
         {children(field.getValue(), field.setValue, field.getErrors())}
-        {field.getErrors()?.map((error, index) => (
-          <Text key={index} style={styles.error}>
-            {error}
-          </Text>
-        ))}
+        {field.getErrors()?.map((error, index) => errorRenderer(error, index))}
       </View>
     );
   }
 );
+
+const FormFieldLabel = ({
+  style,
+  children,
+}: {
+  style?: TextStyle;
+  children: ReactNode;
+}) => {
+  return <Text style={[styles.label, style]}>{children}</Text>;
+};
+
+const FormFieldError = ({
+  style,
+  children,
+}: {
+  style?: TextStyle;
+  children: ReactNode;
+}) => {
+  return <Text style={[styles.error, style]}>{children}</Text>;
+};
 
 const styles = StyleSheet.create({
   fieldContainer: {
